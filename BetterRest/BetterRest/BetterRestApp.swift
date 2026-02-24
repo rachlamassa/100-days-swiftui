e060925b1610bd565fa2c3ef5babed7772bb7ecd
Rachael LaMassa <158332494+rachlamassa@users.noreply.github.com>
2026-02-24 18:34:23 -0500

    Project 4

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Rachael LaMassa on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
